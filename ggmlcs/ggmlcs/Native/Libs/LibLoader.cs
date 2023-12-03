3aab7a595ef295a85130f332c34308eb5d3e68d5
unknown <yrezehi@gmail.com>
2023-12-03 10:11:33 -0800

    sync

---
+++ ggmlcs/ggmlcs/Native/Libs/LibLoader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ggmlcs.Native.Libs
+{
+    public static class LibLoader
+    {
+        public static void LibraryLoad()
+        {
+            LibraryEnvironment libEnv = NativeHelper.GetNativeLibraryEnvironment;
+
+            switch (libEnv.OperatingSystem)
+            {
+                case "win":
+                    WindowsLoader.LibraryLoad(libEnv.LibraryPath);
+                    break;
+                case "linux":
+                    LinuxLoader.LibraryLoad(libEnv.LibraryPath);
+                    break;
+                case "osx":
+                    MacLoader.LibraryLoad(libEnv.LibraryPath);
+                    break;
+            }
+        }
+    }
+}
---
+++ ggmlcs/ggmlcs/Native/Libs/Platforms/WindowsLoader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ggmlcs.Native.Libs.Platforms
+{
+    public static class WindowsLoader
+    {
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr LoadLibrary(string lpFileName);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+
+        public static void LibraryLoad(string libraryPath)
+        {
+            IntPtr libHandle = LoadLibrary(libraryPath);
+            if (libHandle == IntPtr.Zero)
+            {
+                throw new ArgumentException($"Failed to load {libraryPath}");
+            }
+        }
+    }
