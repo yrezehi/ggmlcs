932eae4ba7e96c570d32abd0dd825d1e74c24045
unknown <yrezehi@gmail.com>
2023-12-13 09:02:45 -0800

    setup: prmopt struc

---
+++ ggmlcs/ggmlcs/Native/Binding/Mode/Instruct/Prompt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ggmlcs.Native.Binding.Mode.Instruct
+{
+    internal class Prompt
+    {
+    }
+}
