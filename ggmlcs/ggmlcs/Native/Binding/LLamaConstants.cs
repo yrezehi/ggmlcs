8a397e054d8ef0194672ddfb9524773f0e1aa259
unknown <yrezehi@gmail.com>
2023-12-13 07:45:58 -0800

    setup: LLamaConstants

---
+++ ggmlcs/ggmlcs/Native/Binding/LLamaConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ggmlcs.Native.Binding
+{
+    public class LLamaConstants
+    {
+        const int LLAMA_ROPE_SCALING_UNSPECIFIED = -1;
+        const uint LLAMA_DEFAULT_SEED = 0xFFFFFFFF;
+        const int GGML_DEFAULT_N_THREADS = 4;
+        const int GGML_TYPE_F16 = 1;
+    }
+}
