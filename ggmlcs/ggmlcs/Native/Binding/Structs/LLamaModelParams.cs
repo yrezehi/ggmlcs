c7f253a47994e17bc857ff248f93eeb457e551a7
unknown <yrezehi@gmail.com>
2023-12-13 07:53:29 -0800

    finish: LLamaModelParams

---
+++ ggmlcs/ggmlcs/Native/Binding/Structs/LLamaModelParams.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ggmlcs.Native.Binding.Structs
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct LLamaModelParams
+    {
+        public int n_gpu_layers;
+        public int main_gpu;
+        public IntPtr tensor_split;
+        public IntPtr progress_callback;
+        public IntPtr progress_callback_user_data;
+        public IntPtr kv_overrides;
+        public bool vocab_only;
+        public bool use_mmap;
+        public bool use_mlock;
+    }
+}
