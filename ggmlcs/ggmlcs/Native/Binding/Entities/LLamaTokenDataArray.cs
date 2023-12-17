85d1781aff542bc954916171cc553806cd1caef2
unknown <yrezehi@gmail.com>
2023-12-17 06:29:16 -0800

    add: LLamaTokenDataArray

---
+++ ggmlcs/ggmlcs/Native/Binding/Entities/LLamaTokenDataArray.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ggmlcs.Native.Binding.Entities
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct LLamaTokenDataArray
+    {
+        public LLamaTokenData* data;
+        public ulong size;
+        public bool sorted;
+    }
+}
