45ce845e3ee2e9b6534659fb6c12d32cecc0e72c
unknown <yrezehi@gmail.com>
2023-12-18 08:43:37 -0800

    add: LlamaModelKvOverrideType

---
+++ ggmlcs/ggmlcs/Native/Binding/Entities/LlamaModelKvOverrideType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ggmlcs.Native.Binding.Entities
+{
+    public enum LlamaModelKvOverrideType
+    {
+        Int = 0,
+        Float = 1,
+        Bool = 2
+    }
+}
