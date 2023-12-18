36584ad00fa78e5bb78218af96dadae887158928
unknown <yrezehi@gmail.com>
2023-12-18 08:42:59 -0800

    add: LlamaModelKvOverride

---
+++ ggmlcs/ggmlcs/Native/Binding/Entities/LlamaModelKvOverride.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ggmlcs.Native.Binding.Entities
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct LlamaModelKvOverride
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 128)]
+        public byte[] key;
+
+        public LlamaModelKvOverrideType tag;
+
+        public LlamaModelKvOverrideValue value;
+    }
+}
