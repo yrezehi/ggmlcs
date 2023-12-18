86c3ac77fd62e77ecbac7b7ad8dc9ab76696c1d3
unknown <yrezehi@gmail.com>
2023-12-18 08:38:18 -0800

    add: LlamaProgressCallback

---
+++ ggmlcs/ggmlcs/Native/Binding/Entities/LLamaModelMetadataOverride.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ggmlcs.Native.Binding.Entities
+{
+    public delegate void LlamaProgressCallback(float progress, IntPtr ctx);
+}
