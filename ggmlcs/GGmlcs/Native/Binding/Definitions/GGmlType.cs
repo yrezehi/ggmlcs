df8df78065c5e8100c14929a29dace1154ce1b91
unknown <yrezehi@gmail.com>
2023-12-30 04:38:03 -0800

    setup: GGmlType

--- ggmlcs/GGmlcs/Native/Binding/Definitions/GGmlType.cs
+++ ggmlcs/GGmlcs/Native/Binding/Definitions/GGmlType.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace GGmlcs.Native.Binding.Definitions
 {
-    internal class GGmlType
+    public enum GGmlType
     {
+        GGML_TYPE_F32  = 0,
+        GGML_TYPE_F16  = 1,
+        GGML_TYPE_Q4_0 = 2,
+        GGML_TYPE_Q4_1 = 3,
+        GGML_TYPE_Q5_0 = 6,
+        GGML_TYPE_Q5_1 = 7,
+        GGML_TYPE_Q8_0 = 8,
+        GGML_TYPE_Q8_1 = 9,
+        GGML_TYPE_Q2_K = 10,
+        GGML_TYPE_Q3_K = 11,
+        GGML_TYPE_Q4_K = 12,
+        GGML_TYPE_Q5_K = 13,
+        GGML_TYPE_Q6_K = 14,
+        GGML_TYPE_Q8_K = 15,
+
+        GGML_TYPE_I8,
+        GGML_TYPE_I16,
+        GGML_TYPE_I32,
+        GGML_TYPE_COUNT,
     }
 }
