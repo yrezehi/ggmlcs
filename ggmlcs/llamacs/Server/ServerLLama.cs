ac30252ab50098958d3ad8c2b470280c8e758d83
unknown <yrezehi@gmail.com>
2024-01-01 07:07:09 -0800

    setup: server.llm.attr

--- ggmlcs/llamacs/Server/ServerLLama.cs
+++ ggmlcs/llamacs/Server/ServerLLama.cs
@@ -8,16 +8,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LLamacs.Server.Clients;
+using LLamacs.Native.Binding.Definitions.Batch;
 
 namespace LLamacs.Server
 {
     public class ServerLLama : ILLama<LLamaClient>
     {
+
         private LLamaContext Context { get; set; }
         private LLamaModel Model { get; set; }
 
         private LLamaContextParams ContextParams { get; set; } = new LLamaContextParams();
         private LLamaModelParams ModelParams { get; set; } = new LLamaModelParams();
+
+        public LLamaBatch Batch { get; set; }
+
+        public bool multimodal { get; set; } = false;
+        public bool clean_kv_cache { get; set; } = true;
+        public bool all_slots_are_idl { get; set; } = false;
+        public bool add_bos_token { get; set; } = true;
+
+        public int id_gen { get; set; }
+        public int n_ctx { get; set; }
+
+        public bool system_need_update { get; set; }
+        public string system_prompt { get; set; }
+        public List<LLamaToken> system_tokens { get; set; }
+
+        public string name_user { get; set; }
+        public string name_assistant { get; set; }
+
+        // TODO: ListLLamaClientSlot
+
+        // TODO: List.TaskServer
+
+        // TODO: List.TaskResult
+
+        // TODO: List.multi
+
+        // TODO: mutex.mutex_tasks
+
+        // TODO: condition_variable.condition_tasks
+
+        // TODO: mutex.mutex_results
+
+        // TODO: condition_variable.condition_results
 
         private ServerLLama(LLamaContext context, LLamaModel model, LLamaContextParams contextParams) =>
             (Context, Model, ContextParams) = (context, model, contextParams);
