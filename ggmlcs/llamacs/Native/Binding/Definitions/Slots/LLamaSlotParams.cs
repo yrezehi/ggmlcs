2670c6df3103b0bc87143e8037fee4e3c0d22a4c
unknown <yrezehi@gmail.com>
2024-01-01 07:21:44 -0800

    add: llamaslotparams

---
+++ ggmlcs/llamacs/Native/Binding/Definitions/Slots/LLamaSlotParams.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLamacs.Native.Binding.Definitions.Slots
+{
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public class LLamaSlotParams
+    {
+        public bool stream ;
+        public bool cache_prompt;
+
+        public uint seed;
+        public int n_keep = 0;
+        public int n_predict = -1;
+
+        public List<string> antiprompt;
+
+        public string input_prefix;
+        public string input_suffix;
+
+        public static LLamaSlotParams Default()
+        {
+            return new LLamaSlotParams() {
+                stream = true,
+                cache_prompt = false,
+                seed = uint.MinValue,
+                n_keep = 0,
+                n_predict = -1,
+                antiprompt = new List<string>(),
+                input_prefix = "",
+                input_suffix = ""
+            };
+        }
+    }
+}
