8debcd0bf6f6c333bd0518957c9e7c2c87bef60d
unknown <yrezehi@gmail.com>
2024-01-01 18:39:07 -0800

    setup: LLamaSamplingParams

--- ggmlcs/llamacs/Native/Binding/Definitions/Sampling/LLamaSamplingParams.cs
+++ ggmlcs/llamacs/Native/Binding/Definitions/Sampling/LLamaSamplingParams.cs
@@ -5,24 +5,23 @@ namespace LLamacs.Native.Binding.Definitions.Sampling
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
     public struct LLamaSamplingParams
     {
-        
-            int n_prev                = 64;       // number of previous tokens to remember
-            int n_probs               = 0;        // if greater than 0, output the probabilities of top n_probs tokens.
-            int top_k                 = 40;       // <= 0 to use vocab size
-            float top_p                 = 0.95f;    // 1.0 = disabled
-            float min_p                 = 0.05f;    // 0.0 = disabled
-            float tfs_z                 = 1.00f;    // 1.0 = disabled
-            float typical_p             = 1.00f;    // 1.0 = disabled
-            float temp                  = 0.80f;    // 1.0 = disabled
-            int penalty_last_n        = 64;       // last n tokens to penalize (0 = disable penalty, -1 = context size)
-            float penalty_repeat        = 1.10f;    // 1.0 = disabled
-            float penalty_freq          = 0.00f;    // 0.0 = disabled
-            float penalty_present       = 0.00f;    // 0.0 = disabled
-            int mirostat              = 0;        // 0 = disabled, 1 = mirostat, 2 = mirostat 2.0
-            float mirostat_tau          = 5.00f;    // target entropy
-            float mirostat_eta          = 0.10f;    // learning rate
-            bool        penalize_nl           = true;     // consider newlines as a repeatable token
-            string samplers_sequence     = "kfypmt"; // top_k, tail_free, typical_p, top_p, min_p, temp
+        public int n_prev = 64;
+        public int n_probs = 0;
+        public int top_k = 40;
+        public float top_p = 0.95f;
+        public float min_p = 0.05f;
+        public float tfs_z = 1.00f;
+        public float typical_p = 1.00f;
+        public float temp = 0.80f;
+        public int penalty_last_n = 64;
+        public float penalty_repeat = 1.10f;
+        public float penalty_freq = 0.00f;
+        public float penalty_present = 0.00f;
+        public int mirostat = 0;
+        public float mirostat_tau = 5.00f;
+        public float mirostat_eta = 0.10f;
+        public bool penalize_nl = true;
+        public string samplers_sequence = "kfypmt";
         /*
             std::string grammar;  // optional BNF-like grammar to constrain sampling
 
@@ -36,5 +35,26 @@ namespace LLamacs.Native.Binding.Definitions.Sampling
             std::vector<llama_token> penalty_prompt_tokens;
             bool                     use_penalty_prompt_tokens = false;
          */
+
+        public static LLamaSamplingParams Default() =>
+            new LLamaSamplingParams () {
+                n_prev = 64,
+                n_probs = 0,
+                top_k = 40,
+                top_p = 0.95f,
+                min_p = 0.05f,
+                tfs_z = 1.00f,
+                typical_p = 1.00f,
+                temp = 0.80f,
+                penalty_last_n = 64,
+                penalty_repeat = 1.10f,
+                penalty_freq = 0.00f,
+                penalty_present = 0.00f,
+                mirostat = 0,
+                mirostat_tau = 5.00f,
+                mirostat_eta = 0.10f,
+                penalize_nl = true,
+                samplers_sequence = "kfypmt",
+            };
     }
 }
