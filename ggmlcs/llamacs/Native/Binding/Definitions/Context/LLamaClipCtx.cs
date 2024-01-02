0338fa7853e87c60718707d5cb1988b30b613413
unknown <yrezehi@gmail.com>
2024-01-02 09:36:26 -0800

    finish: LLamaClipCtx

--- ggmlcs/llamacs/Native/Binding/Definitions/Context/LLamaClipCtx.cs
+++ ggmlcs/llamacs/Native/Binding/Definitions/Context/LLamaClipCtx.cs
@@ -1,4 +1,5 @@
-﻿using LLamacs.Native.Binding.Definitions.Vision;
+﻿using LLamacs.Native.Binding.Definitions.GGUF;
+using LLamacs.Native.Binding.Definitions.Vision;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,44 +10,30 @@ using System.Threading.Tasks;
 namespace LLamacs.Native.Binding.Definitions.Context
 {
     [StructLayout(LayoutKind.Sequential)]
-    public struct LLamaClipCtx
+    public unsafe struct LLamaClipCtx
     {
-            public bool has_text_encoder;
-            public bool has_vision_encoder;
-            public bool has_llava_projector;
+        public bool has_text_encoder;
+        public bool has_vision_encoder;
+        public bool has_llava_projector;
         
-            public ClipVisionModel vision_model;
+        public ClipVisionModel vision_model;
 
-            public float[] image_mean;
-            public float[] image_std;
+        public float[] image_mean;
+        public float[] image_std;
 
-            public bool use_gelu;
+        public bool use_gelu;
 
-            public int ftype;
-
+        public int ftype;
             
-            struct gguf_context * ctx_gguf;
-            struct ggml_context * ctx_data;
-            /*
-            std::vector<uint8_t> buf_compute_meta;
+        public GGUFContext* ctx_gguf;
+        public GGMLContext* ctx_data;
 
-            // memory buffers to evaluate the model
-            ggml_backend_buffer_t params_buffer = NULL;
-            ggml_backend_buffer_t compute_buffer = NULL;
-            ggml_backend_t backend = NULL;
-            ggml_allocr * compute_alloc = NULL;
-        */
+        public List<byte> buf_compute_meta;
 
-        public static LLamaClipCtx Default()
-        {
-            return new LLamaClipCtx()
-            {
-                has_text_encoder = false,
-                has_vision_encoder = false,
-                has_llava_projector = false,
-                use_gelu = false,
-                ftype = 1
-            };
-        }
+
+        public GGMLBackendBufferT params_buffer;
+        public GGMLBackendBufferT compute_buffer;
+        public GGMLBackendT backend;
+        public GGMLAllocr* compute_alloc;
     }
 }
--- ggmlcs/llamacs/Native/LLamaGlobalConstants.cs
+++ ggmlcs/llamacs/Native/LLamaGlobalConstants.cs
@@ -4,3 +4,7 @@ global using LLamaModel = System.IntPtr;
 global using LLamaToken = System.Int32;
 global using LLamaPos = System.Int32;
 global using LlamaSeqId = System.Int32;
+
+global using GGMLBackendBufferT = System.IntPtr;
+global using GGMLBackendT = System.IntPtr;
+global using GGMLAllocr = System.IntPtr;
