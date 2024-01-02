2aefd4ea0ad749440f12ec08e1df06bcdba1f841
unknown <yrezehi@gmail.com>
2024-01-02 10:04:27 -0800

    add: GGUFHeader

---
+++ ggmlcs/llamacs/Native/Binding/Definitions/GGUF/GGUFHeader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLamacs.Native.Binding.Definitions.GGUF
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct GGUFHeader
+    {
+        public char[] magic;
+
+        public uint version;
+        public ulong n_tensors; 
+        public ulong n_kv;     
+    }
+}
