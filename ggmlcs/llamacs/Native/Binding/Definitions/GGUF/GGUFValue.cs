f4f15a665d05dec4ff94a7117bf08cfad641bd58
unknown <yrezehi@gmail.com>
2024-01-02 11:19:24 -0800

    add: GGUFValue

---
+++ ggmlcs/llamacs/Native/Binding/Definitions/GGUF/GGUFValue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLamacs.Native.Binding.Definitions.GGUF
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct GGUFValue
+    {
+        public byte uint8;
+        public sbyte int8;
+        public ushort uint16;
+        public short int16;
+        public uint uint32;
+        public int int32;
+        public float float32;
+        public ulong uint64;
+        public long int64;
+        public double float64;
+        public bool bool_;
+
+        public GGUFStr str;
+    }
+}
