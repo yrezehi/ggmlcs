c65001405d35e985c311746afec85861e4ee7d12
unknown <yrezehi@gmail.com>
2024-01-01 18:43:34 -0800

    finish: LLamaSamplingParams

--- ggmlcs/llamacs/Native/Binding/Definitions/Sampling/LLamaSamplingParams.cs
+++ ggmlcs/llamacs/Native/Binding/Definitions/Sampling/LLamaSamplingParams.cs
@@ -22,39 +22,13 @@ namespace LLamacs.Native.Binding.Definitions.Sampling
         public float mirostat_eta = 0.10f;
         public bool penalize_nl = true;
         public string samplers_sequence = "kfypmt";
-        /*
-            std::string grammar;  // optional BNF-like grammar to constrain sampling
 
-            // Classifier-Free Guidance
-            // https://arxiv.org/abs/2306.17806
-            std::string cfg_negative_prompt; // string to help guidance
-            float       cfg_scale     = 1.f; // how strong is guidance
+        public string grammar;
+        public string cfg_negative_prompt;
+        public float cfg_scale = 1f;
+        public IDictionary<LLamaToken, float> logit_bias;
 
-            std::unordered_map<llama_token, float> logit_bias; // logit bias for specific tokens
-
-            std::vector<llama_token> penalty_prompt_tokens;
-            bool                     use_penalty_prompt_tokens = false;
-         */
-
-        public static LLamaSamplingParams Default() =>
-            new LLamaSamplingParams () {
-                n_prev = 64,
-                n_probs = 0,
-                top_k = 40,
-                top_p = 0.95f,
-                min_p = 0.05f,
-                tfs_z = 1.00f,
-                typical_p = 1.00f,
-                temp = 0.80f,
-                penalty_last_n = 64,
-                penalty_repeat = 1.10f,
-                penalty_freq = 0.00f,
-                penalty_present = 0.00f,
-                mirostat = 0,
-                mirostat_tau = 5.00f,
-                mirostat_eta = 0.10f,
-                penalize_nl = true,
-                samplers_sequence = "kfypmt",
-            };
+        public List<LLamaToken> penalty_prompt_tokens;
+        public bool use_penalty_prompt_tokens = false;
     }
 }
