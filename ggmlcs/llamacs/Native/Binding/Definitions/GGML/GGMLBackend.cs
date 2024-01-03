db23223606b7c0b6b51ba5e31a0dd2aef4091fff
unknown <yrezehi@gmail.com>
2024-01-02 21:05:37 -0800

    set: GGMLBackend

---
+++ ggmlcs/llamacs/Native/Binding/Definitions/GGML/GGMLBackend.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLamacs.Native.Binding.Definitions.GGML
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct GGMLBackend
+    {
+        public ggml_backend_i iface;
+
+        ggml_backend_context_t context;
+    }
+}
