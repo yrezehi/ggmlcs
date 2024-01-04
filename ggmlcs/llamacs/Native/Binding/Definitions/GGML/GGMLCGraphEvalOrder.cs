3fbbb14d322ad4da7f9d589c7c895c4d45a07123
unknown <yrezehi@gmail.com>
2024-01-04 06:21:18 -0800

    set: GGMLCGraphEvalOrder

---
+++ ggmlcs/llamacs/Native/Binding/Definitions/GGML/GGMLCGraphEvalOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLamacs.Native.Binding.Definitions.GGML
+{
+    public enum GGMLCGraphEvalOrder
+    {
+        GGML_CGRAPH_EVAL_ORDER_LEFT_TO_RIGHT = 0,
+        GGML_CGRAPH_EVAL_ORDER_RIGHT_TO_LEFT,
+        GGML_CGRAPH_EVAL_ORDER_COUNT
+    }
+}
